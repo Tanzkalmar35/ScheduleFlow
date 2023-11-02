2273e1c9da5b0eec59abc73cb4c19ec3e8da7c2a
Tanzkalmar35 <fabianholler2005@gmail.com>
2023-11-02 20:21:32 +0100

    adding more commands and the first step to function integration

--- src/cli.rs
+++ src/cli.rs
@@ -32,14 +32,40 @@ pub fn cli() -> Command {
                 .arg(
                     Arg::new("to")
                         .long("to")
+                        .short('t')
                         .help("The tiem the event ends")
                         .required(true)
                 )
                 .arg(
-                    Arg::new("COLOR")
+                    Arg::new("color")
                         .long("color")
                         .short('c')
                         .help("The color of the event")
+                )
+        )
+        .subcommand(
+            Command::new("show")
+                .about("Shows the calendar of a specified date period")
+                .arg(
+                    Arg::new("date")
+                        .long("date")
+                        .short('d')
+                        .help("The date that will be used")
+                        .required_unless_present_all(["from", "to"])
+                )
+                .arg(
+                    Arg::new("from")
+                        .long("from")
+                        .short('f')
+                        .help("The starting date of the printed calendar")
+                        .required_unless_present("date")
+                )
+                .arg(
+                    Arg::new("to")
+                        .long("to")
+                        .short('t')
+                        .help("The ending date of the printed calendar")
+                        .required_unless_present("date")
                 )
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::ArgMatches;
+
 mod cli;
 mod calender_endpoint;
 
@@ -9,8 +11,23 @@ fn main() {
             println!(
                 "Following value for 'name' of 'add' was provided: {}",
                 sub_matches.get_one::<String>("name").expect("required")
-            )
+            );
+            add_event(sub_matches);
+        },
+        Some(("show", _sub_matches)) => {
+            println!(
+                "You want to show your calendar? DENIED"
+            );
+            show_event(_sub_matches);
         }
         _ => unreachable!(),
     }
+}
+
+fn add_event(args: &ArgMatches) {
+
+}
+
+fn show_event(args: &ArgMatches) {
+
 }

d9eb5929ebd73668364089e6d6795f73a2f5183c
Tanzkalmar35 <fabianholler2005@gmail.com>
2023-11-09 14:54:01 +0100

    outsourcing into calendar.rs

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,20 +1,9 @@
-use icalendar::CalendarDateTime;
+use clap::ArgMatches;
+use icalendar::{Event, Component};
 
-struct Event {
-    title: String,
-    starting_time: Option<CalendarDateTime>,
-    ending_time: Option<CalendarDateTime>,
-    date: Option<CalendarDateTime>
-}
-
-impl Event {
-    /// Creates a new event
-    fn new(title: String, starting_time: CalendarDateTime, ending_time: CalendarDateTime, date: CalendarDateTime) -> Event {
-        Event {
-            title,
-            starting_time: Some(starting_time),
-            ending_time: Some(ending_time),
-            date: Some(date)
-        }
-    }
+pub fn add_event(cmd_arg: &ArgMatches) -> Event {
+    let name = cmd_arg.get_one::<String>("name").expect("error processing name!");
+    Event::new()
+        .summary(name)
+        .done()
 }

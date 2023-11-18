4b52b93fa1f3b9e65faf19a6d05875c36e5ceaa8
Tanzkalmar35 <fabianholler2005@gmail.com>
2023-11-18 01:13:41 +0100

    working on making the calendar customizable

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,10 +1,15 @@
+use time::Date;
+
 use ratatui::Frame;
 use ratatui::layout::{Layout, Direction, Constraint, Alignment};
 use ratatui::style::{Style, Color};
+use ratatui::widgets::calendar::{Monthly, CalendarEventStore};
 use ratatui::widgets::{Block, Borders};
 
+type Layouts = std::rc::Rc<[ratatui::prelude::Rect]>;
+
 pub fn render(frame: &mut Frame) {
-    let main_layout = Layout::default()
+    let main_layout: Layouts = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
             Constraint::Length(1),
@@ -12,6 +17,7 @@ pub fn render(frame: &mut Frame) {
             Constraint::Length(1),
         ])
         .split(frame.size());
+
     frame.render_widget(
         Block::new()
             .borders(Borders::TOP)
@@ -21,18 +27,20 @@ pub fn render(frame: &mut Frame) {
             .fg(Color::Yellow)), // TODO: Make the coor adjustable
         main_layout[0],
     );
+    render_calendar_month(frame, &main_layout, time::Month::April, 1);
+    render_calendar_month(frame, &main_layout, time::Month::May, 1);
+    render_calendar_month(frame, &main_layout, time::Month::June, 1);
+}
 
-    let inner_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(100)])
-        .split(main_layout[1]);
+fn render_calendar_month(frame: &mut Frame, main_layout: &Layouts, month: time::Month, idx: usize) {
     frame.render_widget(
-        Block::new()
-            .borders(Borders::ALL)
-            .title(" INNER ")
-            .title_alignment(Alignment::Center)
-            .style(Style::new()
-            .fg(Color::Yellow)), // TODO: Make the coor adjustable
-        inner_layout[0],
-    );
+        Monthly::new(
+            Date::from_calendar_date(2023, month, 1).unwrap(),
+            CalendarEventStore::default(),
+        )
+        .show_weekdays_header(Style::default())
+        .show_month_header(Style::default())
+        .show_surrounding(Style::default()),
+        main_layout[idx]
+    )
 }

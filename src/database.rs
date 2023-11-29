d5dd1f70e8fe791e8e925808bcc4a96ea7c81dd7
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-11-29 23:08:26 +0100

    removed a comment that was not needed.

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,6 @@
+use anyhow::Result;
 use surrealdb::engine::local::Mem;
 use surrealdb::Surreal;
-
-use anyhow::Result;
 
 type Database = Surreal<surrealdb::engine::local::Db>;
 
@@ -24,19 +23,11 @@ impl SurrealDbBuilder {
     async fn build() -> Result<SurrealDb> {
         let db = Surreal::new::<Mem>(()).await?;
         db.use_ns("test").use_db("test").await?;
-        
+
         Ok(
             SurrealDb {
                 datastore: db,
             }
-          )
-
-
-        //let created = db
-        //    .create("person")
-        //    .content(
-        //        Person::new("Mr.", "John Doe", true)
-        //    ).await?;
-        //dbg!(created);
+        )
     }
 }

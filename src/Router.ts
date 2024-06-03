106ceaef2a189755ed740dfadf178e50e220f914
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2024-06-03 22:56:28 +0200

    Continued designing the login page.
    Began implementing the login logic.

--- src/Router.ts
+++ src/Router.ts
@@ -10,3 +10,15 @@ function navigateToIndexPage() {
 function navigateToLoginPage() {
     navigateTo("../src/login.html")
 }
+
+function submitLoginForm() {
+    const loginUsername = document.getElementById("login-modal-username"),
+        loginEmail = document.getElementById("login-modal-email"),
+        loginPassword = document.getElementById("login-modal-password")
+
+    let result = {
+        username: loginUsername,
+        email: loginEmail,
+        password: loginPassword
+    }
+}
